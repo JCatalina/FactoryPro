3c0dcc689631f9b8c34f4908d642e369607e55f4
lwj <453786790@qq.com>
2019-04-07 14:55:42 +0800

    二次修改机器倍率
    二次修改机器倍率

--- src/com/factory/common/Constants.java
+++ src/com/factory/common/Constants.java
@@ -5,8 +5,8 @@ public interface Constants {
 	
 	public static int MACHINE_NUM=20;
 	public static final int ERROR_CODE = 404;
-	public static final double LITTLE_PERCENTAGE= 0.005; //小倍率
-	public static final double BIG_PERCENTAGE= 0.0058; //大倍率
+	public static final double LITTLE_PERCENTAGE= 0.0052; //小倍率
+	public static final double BIG_PERCENTAGE= 0.0059; //大倍率
 	public static final double MIDDLE_PERCENTAGE= 0.0052; //中倍率
 	//////////////////////////////////////////////
 	
